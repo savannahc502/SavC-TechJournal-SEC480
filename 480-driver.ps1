b5b1ab13e3bcdd30efe0d01244baf082a5467a67
savannahc502 <savannah.ciak@mymail.champlain.edu>
2026-03-03 03:23:40 -0500

    New additions

---
+++ 480-driver.ps1
@@ -0,0 +1,11 @@
+#Load the Module
+Import-Module '480-utils' -Force 
+
+# Load Configurations from JSON File. Get-480Config is stored in 480-utils.psm1
+$conf = Get-480Config -config_path "/home/savannah_local/SavC-TechJournal-SEC480/480.json"
+
+# Connecting to VCenter using the JSON value stored in vcenter_server
+480_connect -server $conf.vcenter_server
+
+# List other Modules to Run. Comment out what's not needed
+# 480_banner
--- modules/480-utils/480-utils.psm1
+++ modules/480-utils/480-utils.psm1
@@ -2,3 +2,28 @@ function 480_banner()
 {
     Write-Host "Hello 480 Besties!"
 }
+
+function 480_connect([string] $server)
+{
+    $connect = $global:DefaultVIServer
+    if ($connect){
+        $msg = "Already connected to: {0}" -f $connect
+        Write-Host -ForegroundColor Green $msg
+    }else {
+        $connect = Connect-VIServer -Server $server
+    }
+}
+
+function Get-480Config([string] $config_path)
+{
+    Write-Host "Reading Configurations"
+    $conf = $null
+    if(Test-Path $config_path){
+        Write-Host -ForegroundColor "green" "Configuration found."
+        $conf = (Get-Content -Raw -Path $conf_path | ConnvertFrom-JSON)
+    }
+    else{
+        Write-Host -ForegroundColor "yellow" "No configuration found."
+    }
+    return $conf
+}
