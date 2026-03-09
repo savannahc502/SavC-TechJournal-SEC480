d56ee490a8acdc8d8e088d770adc878d5026bc0d
Savannah C <113316987+savannahc502@users.noreply.github.com>
2026-03-08 23:46:18 -0400

    Update 480-driver.ps1

--- 480-driver.ps1
+++ 480-driver.ps1
@@ -1,4 +1,5 @@
-# // Load the Module // 
+# // Load VMware PowerCLI and Module //
+Import-Module VMware.PowerCLI
 Import-Module '480-utils' -Force 
 
 # // Load Configurations from JSON File. Get-480Config is stored in 480-utils.psm1 //
@@ -10,16 +11,43 @@ $conf = Get-480Config -config_path "/home/savannah_loc/SavC-TechJournal-SEC480/4
 # // Connecting to VCenter using the JSON value stored in vcenter_server //
 480_connect -server $conf.vcenter_server
 
-# // Select VM //
-Write-Host ""
-Write-Host "Now selecting a VM from the folder $($conf.output_folder)"
-Select-VM -folder $conf.output_folder
+# // 480 Main Menu //
+while ($true) {
+    Write-Host ""
+    Write-Host "          480 MENU            " -ForegroundColor Cyan
+    Write-Host "1) Create a New Network"
+    Write-Host "2) Clone a VM"
+    Write-Host "3) Get the IP and MAC of a VM"
+    Write-Host "4) Exit"
+    Write-Host ""
 
-# // Create a New VM //
-New-480Clone -conf $conf
+    $choice = Read-Host "Enter your choice (1-4)"
 
-# // Create a new Network //
-New-Network -conf $conf
+    switch ($choice) {
+        "1" {
+            Write-Host ""
+            Write-Host "// Create a New Network //" -ForegroundColor Yellow
+            New-Network -conf $conf
+        }
+        "2" {
+            Write-Host ""
+            Write-Host "// Create a New VM //" -ForegroundColor Yellow
+            New-480Clone -conf $conf
+        }
+        "3" {
+            Write-Host ""
+            Write-Host "// Get the IP and MAC of a VM //" -ForegroundColor Yellow
+            Get-IP
+        }
+        "4" {
+            Write-Host ""
+            Write-Host "// Exiting Program //" -ForegroundColor Green
+            break
+        }
 
-# // Get the IP and MAC of a VM //
-Get-IP
+        default {
+            Write-Host ""
+            Write-Host "Invalid selection. Try again." -ForegroundColor Red
+        }
+    }
+}
